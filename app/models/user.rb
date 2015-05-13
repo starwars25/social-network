b83324fb506209e3464aab98cb0d41963f5ae38c
Alexander <a.starwars.d@gmail.com>
2015-05-13 13:16:49 +0300

    Added uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true, length: {minimum: 4, maximum: 20}
+  before_save :downcase
+  validates :username, presence: true, length: {minimum: 4, maximum: 20}, uniqueness: {case_sensitive: false}
   VALID_EMAIL_REGEX = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
-  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX, multiline: true}
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX, multiline: true}, uniqueness: {case_sensitive: false}
+
+
+
+  private
+  def downcase
+    self.username = username.downcase
+    self.email = email.downcase
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,6 +5,10 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
 
+  def setup
+    @user = User.new(username: 'example', email: 'foobar@bar.com')
+  end
+
   test "should create user" do
     assert_difference 'User.count', 1 do
       User.create(username: 'foobar', email: 'foobar@foo.com')
@@ -44,4 +48,47 @@ class UserTest < ActiveSupport::TestCase
       assert_not  user.valid?
     end
   end
+
+  test "username must be unique" do
+    assert_difference 'User.count', 1 do
+      User.create(username: 'foobar', email: 'foobar@foo.com')
+    end
+    assert_no_difference 'User.count' do
+      User.create(username: 'foobar', email: 'foobar@bar.com')
+    end
+  end
+
+  test "email must be unique" do
+    assert_difference 'User.count', 1 do
+      User.create(username: 'foobar', email: 'foobar@foo.com')
+    end
+    assert_no_difference 'User.count' do
+      User.create(username: 'foobaz', email: 'foobar@foo.com')
+    end
+  end
+
+  test "downcase test" do
+    assert_difference 'User.count', 1 do
+      User.create(username: 'foobar', email: 'foobar@foo.com')
+    end
+    assert_no_difference 'User.count' do
+      User.create(username: 'Foobar', email: 'foobar@bar.com')
+    end
+  end
+
+  test "email downcase test" do
+    assert_difference 'User.count', 1 do
+      User.create(username: 'foobar', email: 'foobar@foo.com')
+    end
+    assert_no_difference 'User.count' do
+      User.create(username: 'foobaz', email: 'Foobar@foo.com')
+    end
+  end
+
+  # test "downcase test" do
+  #   duplicate_user = @user.dup
+  #   duplicate_user.email.upcase!
+  #   @user.save
+  #   assert duplicate_user.valid?
+  # end
 end
