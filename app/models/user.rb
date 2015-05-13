146f7b3720097c0e48074102c15494e410991587
Alexander <a.starwars.d@gmail.com>
2015-05-13 12:35:36 +0300

    Model perfectly adjusted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, length: {minimum: 4, maximum: 20}
+  VALID_EMAIL_REGEX = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX, multiline: true}
 end
