87c875de4624c0c04c8be0f8a4f18920e7eee8be
Alexander <a.starwars.d@gmail.com>
2015-05-13 12:15:22 +0300

    Done landing page

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  test 'should get home' do
+    get :home
+    assert_select 'h1'
+    assert_match 'Landing page', response.body
+  end
+end
