2b4ac0b8398a0a358c4ecc1f2931209348e23e64
Alexander <a.starwars.d@gmail.com>
2015-05-13 12:29:56 +0300

    Perfect test suite

---
+++ test/models/user_test.rb
@@ -0,0 +1,47 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "should create user" do
+    assert_difference 'User.count', 1 do
+      User.create(username: 'foobar', email: 'foobar@foo.com')
+    end
+  end
+
+  test "should not create user" do
+    assert_no_difference 'User.count' do
+      User.create(username: '', email: '')
+    end
+  end
+
+  test "username must be long enough" do
+    assert_no_difference 'User.count' do
+      User.create(username: 'foo', email: 'foobar@foo.com')
+    end
+  end
+
+  test "username must not be too long" do
+    assert_no_difference 'User.count' do
+      User.create(username: 'a'*21, email: 'foobar@foo.com')
+    end
+  end
+
+  test "valid emails" do
+    emails = %w(foo@bar.com foobar@foo.org foobar@foo.ru)
+    emails.each do |email|
+      user = User.new(username: 'foobar', email: email)
+      assert  user.valid?
+    end
+  end
+
+  test "invalid emails" do
+    emails = %w(@bar.com, foobar, foo@bar)
+    emails.each do |email|
+      user = User.new(username: 'foobar', email: email)
+      assert_not  user.valid?
+    end
+  end
+end
