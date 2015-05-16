b1b0b70babf8c900372884fe977f70f6dd696ac5
Alexander <a.starwars.d@gmail.com>
2015-05-16 15:10:54 +0300

    Little fix

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -55,6 +55,6 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
                   password_confirmation: "foobaz" }
     assert is_logged_in?
     assert_not flash.empty?
-    assert_redirected_to user
+    assert_redirected_to root_url
   end
 end
