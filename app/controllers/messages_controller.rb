7fe4b933ee66449419d8eee3083a6122e784455b
Alexander <a.starwars.d@gmail.com>
2015-05-18 16:41:35 +0300

    Working chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,25 @@
 class MessagesController < ApplicationController
+  before_action :valid_user_and_dialog, only: [:create]
+  before_action :is_logged_in?
+
+  def create
+    message = Message.new(message_params)
+    if message.save
+      redirect_to dialog_path(params[:message][:dialog_id])
+    else
+      redirect_to root_url
+    end
+
+  end
+
+  private
+  def valid_user_and_dialog
+    user = User.find_by(id: params[:message][:user_id])
+    dialog = Dialog.find_by(id: params[:message][:dialog_id])
+    redirect_to root_url unless dialog.members.include?(user) && user.dialogs.include?(dialog)
+  end
+
+  def message_params
+    params.require(:message).permit(:user_id, :dialog_id, :content)
+  end
 end
--- test/integration/send_message_test.rb
+++ test/integration/send_message_test.rb
@@ -4,4 +4,29 @@ class SendMessageTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @admin = users(:admin)
+    @user_one = users(:user_one)
+    @user_two = users(:user_two)
+    @dialog = dialogs(:one)
+  end
+
+  test 'login and send' do
+    log_in_intergration(@admin, 'admin')
+    assert is_logged_in?
+    get dialog_path(@dialog.id)
+    assert_template('dialogs/show')
+    assert_difference 'Message.count', 1 do
+      post messages_path, message: { user_id: @admin.id, dialog_id: @dialog.id, content: 'Lorem ipsum' }
+    end
+    assert @dialog.messages.count == 1
+    delete logout_path
+    log_in_intergration(@user_two)
+    assert is_logged_in?
+    assert_no_difference 'Message.count', 1 do
+      post messages_path, message: { user_id: @user_two.id, dialog_id: @dialog.id, content: 'Lorem ipsum' }
+    end
+    assert_redirected_to root_url
+  end
 end
