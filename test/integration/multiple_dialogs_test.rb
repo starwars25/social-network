eef5094e34aa3694322fdd103bd05ae173d593ad
Alexander <a.starwars.d@gmail.com>
2015-05-18 19:53:03 +0300

    Dialog test

---
+++ test/integration/multiple_dialogs_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class MultipleDialogsTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def setup
+    @admin = users(:admin)
+    @user_one = users(:user_one)
+    @user_two = users(:user_two)
+  end
+
+  test 'create multidialog' do
+    log_in_intergration @admin, 'admin'
+    assert is_logged_in?
+    assert_difference 'Dialog.count', 1 do
+      post dialogs_path, multi: true, dialog: { string: "#{@user_one.email} #{@user_two.email}", creator_id: @admin.id }
+    end
+    dialog = Dialog.last
+    assert dialog.members.count == 3
+    assert dialog.members.include? @admin
+    assert dialog.members.include? @user_one
+    assert dialog.members.include? @user_two
+    assert @admin.dialogs.include? dialog
+    assert @user_one.dialogs.include? dialog
+    assert @user_two.dialogs.include? dialog
+  end
+end
