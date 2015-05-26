198b7e383421b97b9694d3a51f5936602089be1d
Alexander <a.starwars.d@gmail.com>
2015-05-26 14:04:14 +0300

    Made little fix

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -2,7 +2,7 @@ class FriendRequestsController < ApplicationController
   after_action :send_notification, only: [:create]
   after_action :delete_notification, only: [:update]
   def create
-    FriendRequest.create(from_id: params[:from_id], to_id: params[:to_id])
+    FriendRequest.create(from_id: params[:from_id], to_id: params[:to_id]) unless User.find(params[:from_id]) == User.find(params[:to_id])
     redirect_to User.find_by(id: params[:to_id])
   end
 
